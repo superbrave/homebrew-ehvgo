44f3e74848be3c6fd5c54010e9bab0865b3c4584
Jeroen Bleijenberg <jeb@ehvg.nl>
2024-11-20 16:55:49 +0100

    Only base flask on arch not on os

--- Formula/ehvgo.rb
+++ Formula/ehvgo.rb
@@ -4,20 +4,18 @@ class Ehvgo < Formula
   homepage "https://github.com/superbrave/ehvgo"
   license ""
 
-  on_linux do
-    on_intel do
-      url "https://dist.ehealthsystems.nl/ehvgo/ehvgo-1.0.0-linux-amd64.gz"
-      sha256 "c10cb6447b813f5947a603c1c82d7eb4b67c3ae9143d33316111225d915db85e"
-      def install
-        bin.install "ehvgo-1.0.0-linux-amd64" => "ehvgo"
-      end
+  on_intel do
+    url "https://dist.ehealthsystems.nl/ehvgo/ehvgo-1.0.0-linux-amd64.gz"
+    sha256 "c10cb6447b813f5947a603c1c82d7eb4b67c3ae9143d33316111225d915db85e"
+    def install
+      bin.install "ehvgo-1.0.0-linux-amd64" => "ehvgo"
     end
-    on_arm do
-      url "https://dist.ehealthsystems.nl/ehvgo/ehvgo-1.0.0-linux-arm64.gz"
-      sha256 "34d99d94066d2b09c7e4f82078e82970b155f5558113cf0fd0748d9f5cfdea4a"
-      def install
-        bin.install "ehvgo-1.0.0-linux-arm64" => "ehvgo"
-      end
+  end
+  on_arm do
+    url "https://dist.ehealthsystems.nl/ehvgo/ehvgo-1.0.0-linux-arm64.gz"
+    sha256 "34d99d94066d2b09c7e4f82078e82970b155f5558113cf0fd0748d9f5cfdea4a"
+    def install
+      bin.install "ehvgo-1.0.0-linux-arm64" => "ehvgo"
     end
   end
 
