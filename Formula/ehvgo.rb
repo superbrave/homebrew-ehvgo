fedcfca6b8da499960e399fd4105cc48a9c8badf
Jeroen Bleijenberg <jeb@ehvg.nl>
2024-11-20 16:42:48 +0100

    Updated formula

--- Formula/ehvgo.rb
+++ Formula/ehvgo.rb
@@ -1,38 +1,27 @@
 class Ehvgo < Formula
   desc "EHVG toolbelt"
-  version "0.1.13"
+  version "1.0.0"
   homepage "https://github.com/superbrave/ehvgo"
-  url "https://dist.ehealthsystems.nl/ehvgo/ehvgo-0.1.13-linux-amd64.gz"
-  sha256 "02a79dd745c1d658b5c8a373845450afa1f5b58734d3f9c5d8f4f12919005f07"
   license ""
 
   on_linux do
     on_intel do
-      url "https://dist.ehealthsystems.nl/ehvgo/ehvgo-0.1.13-linux-amd64.gz"
-      sha256 "02a79dd745c1d658b5c8a373845450afa1f5b58734d3f9c5d8f4f12919005f07"
+      url "https://dist.ehealthsystems.nl/ehvgo/ehvgo-1.0.0-linux-amd64.gz"
+      sha256 "c10cb6447b813f5947a603c1c82d7eb4b67c3ae9143d33316111225d915db85e"
       def install
-        bin.install "ehvgo-0.1.13-linux-amd64" => "ehvgo"
+        bin.install "ehvgo-1.0.0-linux-amd64" => "ehvgo"
       end
     end
     on_arm do
-      url "https://dist.ehealthsystems.nl/ehvgo/ehvgo-0.1.13-linux-arm64.gz"
-      sha256 "e6b1f8665d8924f52c5603cf7114c2db489dfc7f048458140ebe090ed177c529"
+      url "https://dist.ehealthsystems.nl/ehvgo/ehvgo-1.0.0-linux-arm64.gz"
+      sha256 "34d99d94066d2b09c7e4f82078e82970b155f5558113cf0fd0748d9f5cfdea4a"
       def install
-        bin.install "ehvgo-0.1.13-linux-arm64" => "ehvgo"
+        bin.install "ehvgo-1.0.0-linux-arm64" => "ehvgo"
       end
     end
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test ehvgo`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system bin/"program", "do", "something"`.
     system bin/"ehvgo" "version"
   end
 end
