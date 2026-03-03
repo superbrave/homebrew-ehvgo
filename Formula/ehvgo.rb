7ed6210aaf11b4aa5f39263d81db84106f1e08ee
Jeroen Bleijenberg <jbleijenberg@ehvg.nl>
2026-03-03 10:37:48 +0100

    Update formula SHA256 hashes

--- Formula/ehvgo.rb
+++ Formula/ehvgo.rb
@@ -6,20 +6,20 @@ class Ehvgo < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/superbrave/ehvgo/releases/download/#{version}/ehvgo-darwin-arm64"
-      sha256 "65d41a67e1f3eb249f648fb4c5b7e31defe005a19fd20d7f2097a4724f38ee16"
+      sha256 "f612b60899b83fd619d9f94f506b01e68a0fb134263ea1701c8a33a722a46045"
     else
       url "https://github.com/superbrave/ehvgo/releases/download/#{version}/ehvgo-darwin-amd64"
-      sha256 "c4adc96625c2646d0c9b4ee0737e20d6a0f35725cca77f9a36ae81635c520956"
+      sha256 "ba94d41accbe1815930a183a68ec7e6a39cb22a0662dac1e36438de64b7655fe"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/superbrave/ehvgo/releases/download/#{version}/ehvgo-linux-arm64"
-      sha256 "cf63b65efad9216bd4ab01e88fce1e15811e19111deaa25889cfec169d7ba05e"
+      sha256 "132588f4ed658b3c8d0bcddbcaf0a4f670d3b30bb219b8738dc4ae87c7fd8991"
     else
       url "https://github.com/superbrave/ehvgo/releases/download/#{version}/ehvgo-linux-amd64"
-      sha256 "e7ecc013ee767e2d6d2b99c6852f761826841ca00cfaa04740454d9aac741279"
+      sha256 "1ffa87664de1698ef1fb99b5ef36b9ec4257760372801bfe5291c6abc3604a09"
     end
   end
 
